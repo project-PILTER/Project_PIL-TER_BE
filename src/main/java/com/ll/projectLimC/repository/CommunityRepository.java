7efe7f568797579f991d9bc18f285cb481fc35c7
JeUk Park <77144006+sleo113@users.noreply.github.com>
2026-06-03 12:40:51 +0900

    feat: 리포지토리 생성 - CommunityRepository

---
+++ src/main/java/com/ll/projectLimC/repository/CommunityRepository.java
@@ -0,0 +1,8 @@
+package com.ll.projectLimC.repository;
+
+import com.ll.projectLimC.entity.Article.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommunityRepository extends JpaRepository<Article, Long> {
+
+}
