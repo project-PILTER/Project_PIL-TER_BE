5652f45ac74001e6065a5c93a6b73660bb554ab6
JeUk Park <77144006+sleo113@users.noreply.github.com>
2026-06-06 15:28:50 +0900

    feat: CommunityService에 updateCommunityArticle 메서드 추가

--- src/main/java/com/ll/projectLimC/domain/community/service/CommunityService.java
+++ src/main/java/com/ll/projectLimC/domain/community/service/CommunityService.java
@@ -1,10 +1,12 @@
 package com.ll.projectLimC.domain.community.service;
 
 import com.ll.projectLimC.domain.community.dto.CommunityArticleCreateForm;
+import com.ll.projectLimC.domain.community.dto.UpdateCommunityArticleRequest;
 import com.ll.projectLimC.domain.community.entity.CommunityArticle.CommunityArticle;
 import com.ll.projectLimC.domain.community.repository.CommunityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -26,5 +28,16 @@ public class CommunityService {
     // 커뮤니티 게시글 삭제용 메서드
     public void deleteCommunityArticle(long id){
         communityRepository.deleteById(id);
+    }
+
+    // 커뮤니티 게시글 수정용 메서드
+    @Transactional
+    public CommunityArticle updateCommunityArticle(long id, UpdateCommunityArticleRequest request){
+        CommunityArticle communityArticle = communityRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found:" + id));
+
+        communityArticle.updateCommunityArticle(request.getTitle(), request.getContent());
+
+        return communityArticle;
     }
 }
