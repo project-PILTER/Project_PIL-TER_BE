04a207dbc2889646ce894eba3bd7f09db5f57c36
JeUk Park <77144006+sleo113@users.noreply.github.com>
2026-06-06 15:56:20 +0900

    fix: CommunityArticleResponse 수정

--- src/main/java/com/ll/projectLimC/domain/community/dto/CommunityArticleResponse.java
+++ src/main/java/com/ll/projectLimC/domain/community/dto/CommunityArticleResponse.java
@@ -1,13 +1,16 @@
 package com.ll.projectLimC.domain.community.dto;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.ll.projectLimC.domain.community.entity.CommunityArticle.CommunityArticle;
 import lombok.Getter;
 
 @Getter
+@JsonPropertyOrder({ "title", "content" })
 public class CommunityArticleResponse {
     private String title;
     private String content;
 
+    @JsonPropertyOrder({ "title", "content" })
     public CommunityArticleResponse(CommunityArticle communityArticle){
         this.title = communityArticle.getTitle();
         this.content = communityArticle.getContent();
