3b6e0867edbda942d10e783a39174f7c08a3544e
JeUk Park <77144006+sleo113@users.noreply.github.com>
2026-06-11 13:21:04 +0900

    feat: 소셜로그인 - OAuth2 관련 리포지토리 등등 구현 및 수정

--- src/main/java/com/ll/projectLimC/domain/service/UserService.java
+++ src/main/java/com/ll/projectLimC/domain/service/UserService.java
@@ -4,6 +4,7 @@ import com.ll.projectLimC.domain.dto.AddUserRequest;
 import com.ll.projectLimC.domain.entity.User.User;
 import com.ll.projectLimC.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +12,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
 
     public Long save(AddUserRequest dto){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         return userRepository.save(User.builder()
                 .email(dto.getEmail())
-                .password(passwordEncoder.encode(dto.getPassword()))
+                .password(encoder.encode(dto.getPassword()))
                 .build()).getId();
     }
 
---
+++ src/main/java/com/ll/projectLimC/global/config/WebOAuthSecurityConfig.java
@@ -0,0 +1,101 @@
+package com.ll.projectLimC.global.config;
+
+import com.ll.projectLimC.domain.repository.RefreshTokenRepository;
+import com.ll.projectLimC.domain.service.UserService;
+import com.ll.projectLimC.global.config.oauth.OAuth2UserCustomService;
+import com.ll.projectLimC.global.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.boot.security.autoconfigure.web.servlet.PathRequest.toH2Console;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebOAuthSecurityConfig {
+    private final OAuth2UserCustomService oAuth2UserCustomService;
+    private final JwtTokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserService userService;
+
+    @Bean
+    public WebSecurityCustomizer configure(){
+        return (web)->web.ignoring()
+                .requestMatchers(toH2Console())
+                .requestMatchers(
+                        "/img/**",
+                        "/css/**",
+                        "/js/**"
+                );
+    }
+
+    // 토큰 방식으로 인증을 하기에 기존에 사용하던 폼 로그인, 세션 비활성화
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .sessionManagement(management -> management.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
+                // 헤더를 확인할 커스텀 필터 추가
+                .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                // 토큰 재발급 URL은 인증 없이 접근 가능하도록 설정. 나머지 API URL은 인증 필요
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/token").permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll())
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login")
+                        // Authorization 요청과 관련된 상태 저장
+                        .authorizationEndpoint(authorizationEndpoint ->
+                                authorizationEndpoint.authorizationRequestRepository(
+                                oAuth2AuthorizationRequestBasedOnCookieRepository()))
+                        .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(oAuth2UserCustomService))
+                        // 인증 성공 시 실행할 핸들러
+                        .successHandler(oAuth2SuccessHandler())
+                )
+                // /api로 시작하는 url인 경우 401 상태 코드를 반환하도록 예외 처리
+                .exceptionHandling(ex -> ex
+                        .defaultAuthenticationEntryPointFor(
+                                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED,
+                                        request -> request.getRequestURI().startsWith("/api/"))
+                        )
+                )
+                .build();
+    }
+
+    @Bean
+    public OAuth2SuccessHandler oAuthSuccessHandler(){
+        return new OAuth2SuccessHandler(tokenProvider,
+                refreshTokenRepository,
+                oAuth2AuthorizationRequestBasedOnCookieRepository(),
+                userService);
+    }
+
+    @Bean
+    public TokenAuthentiocationFilter tokenAuthentiocationFilter(){
+        return new TokenAuthentiocationFilter(tokenProvider);
+    }
+
+    @Bean
+    public oAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository(){
+        return new oAuth2AuthorizationRequestBasedOnCookieRepository();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
