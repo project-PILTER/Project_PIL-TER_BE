b7589427c1783f4dad56be07ca3425c997c9a21c
JeUk Park <77144006+sleo113@users.noreply.github.com>
2026-06-15 11:48:09 +0900

    Swagger Ui - Request/Response 파일들을 수정

--- src/main/java/com/ll/projectLimC/domain/dto/CreateAccessTokenResponse.java
+++ src/main/java/com/ll/projectLimC/domain/dto/CreateAccessTokenResponse.java
@@ -1,10 +1,14 @@
 package com.ll.projectLimC.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Schema(description = "Access Token 신규 발급 응답 결과")
 public class CreateAccessTokenResponse {
+    @Schema(description = "새로 발급된 Access Token (모든 API 요청 헤더 Authorization에 사용)",
+            example = "asldfkjhqwhroqUIHJOADH...")
     private String accessToken;
 }
