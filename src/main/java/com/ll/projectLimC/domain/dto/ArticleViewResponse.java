3c0cd9fa3ef6ed495b167fbf96f96f579c456ae0
JeUk Park <77144006+sleo113@users.noreply.github.com>
2026-06-16 12:11:56 +0900

    feat: Comment - ArticleViewResponse 파일 수정

--- src/main/java/com/ll/projectLimC/domain/dto/ArticleViewResponse.java
+++ src/main/java/com/ll/projectLimC/domain/dto/ArticleViewResponse.java
@@ -1,10 +1,12 @@
 package com.ll.projectLimC.domain.dto;
 
+import com.ll.projectLimC.domain.entity.Comment.Comment;
 import com.ll.projectLimC.domain.entity.CommunityArticle.CommunityArticle;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -15,6 +17,7 @@ public class ArticleViewResponse {
     private LocalDateTime createdAt;
     private String author;
     private String imageUrl;
+    private List<Comment> comments;
 
     public ArticleViewResponse(CommunityArticle article){
         this.id = article.getId();
@@ -23,5 +26,6 @@ public class ArticleViewResponse {
         this.author = article.getAuthor();
         this.imageUrl = article.getImageUrl();
         this.createdAt = article.getCreatedAt();
+        this.comments = article.getComments();
     }
 }
