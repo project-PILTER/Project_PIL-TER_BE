8bc002d728e8a5597c29f36427ea733439001b68
JeUk Park <77144006+sleo113@users.noreply.github.com>
2026-06-18 11:40:15 +0900

    feat: isHot 기능 구현 - CommunityRepository 수정

--- src/main/java/com/ll/projectLimC/domain/repository/CommunityRepository.java
+++ src/main/java/com/ll/projectLimC/domain/repository/CommunityRepository.java
@@ -2,7 +2,13 @@ package com.ll.projectLimC.domain.repository;
 
 import com.ll.projectLimC.domain.entity.CommunityArticle.CommunityArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 
 public interface CommunityRepository extends JpaRepository<CommunityArticle, Long> {
-
+    // 좋아요(likes) 개수가 많은 순서대로 상위 5개 게시글을 가져오는 쿼리
+    @Query("SELECT a FROM CommunityArticle a ORDER BY SIZE(a.likes) DESC")
+    List<CommunityArticle> findTop5ByOrderByLikeDesc();
 }
