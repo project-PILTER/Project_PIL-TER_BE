69516c907df15ad845b807acb8318fd418ee30d2
JeUk Park <77144006+sleo113@users.noreply.github.com>
2026-06-22 13:59:25 +0900

    feat: 건강일지 기능 구현 - HealthJournalRepository 수정

--- src/main/java/com/ll/projectLimC/domain/healthJournal/repository/HealthJournalRepository.java
+++ src/main/java/com/ll/projectLimC/domain/healthJournal/repository/HealthJournalRepository.java
@@ -1,5 +1,6 @@
 package com.ll.projectLimC.domain.healthJournal.repository;
 
+import com.ll.projectLimC.domain.User.entity.User;
 import com.ll.projectLimC.domain.healthJournal.entity.HealthJournal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,5 +8,5 @@ import java.util.List;
 
 public interface HealthJournalRepository extends JpaRepository<HealthJournal, Long> {
     // 나중에 유저별로 일지 목록을 가져올 때 사용할 수 있기에 미리 선언
-    List<HealthJournal> findByUserIdOrderByJournalDateDesc(Long userId);
+    List<HealthJournal> findByUserIdOrderByJournalDateDesc(User user);
 }
