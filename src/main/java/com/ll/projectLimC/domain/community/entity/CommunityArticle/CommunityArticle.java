be54c1c2715e3401d8ef0ca4e3c77ef2e838879d
JeUk Park <77144006+sleo113@users.noreply.github.com>
2026-06-26 11:27:59 +0900

    fix: CommunityArticle 수정 - 임시저장 기능 관련

--- src/main/java/com/ll/projectLimC/domain/community/entity/CommunityArticle/CommunityArticle.java
+++ src/main/java/com/ll/projectLimC/domain/community/entity/CommunityArticle/CommunityArticle.java
@@ -1,6 +1,7 @@
 package com.ll.projectLimC.domain.community.entity.CommunityArticle;
 
 import com.ll.projectLimC.domain.comment.entity.Comment;
+import com.ll.projectLimC.domain.community.ArticleStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -45,25 +46,33 @@ public class CommunityArticle {
     @OneToMany(mappedBy = "communityArticle", cascade = CascadeType.REMOVE)
     private List<Comment> comments;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private ArticleStatus status = ArticleStatus.PUBLISHED; // 기본값 지정
 
     @Builder
     public CommunityArticle(String author,
                             String title,
                             String content,
-                            String imageUrl
+                            String imageUrl,
                             // String nickname
+                            ArticleStatus status
                             ){
         this.author = author;
         this.title = title;
         this.content = content;
         this.imageUrl = imageUrl;
         // this.nickname = nickname;
+        this.status = status != null ? status : ArticleStatus.PUBLISHED; // 상태값 추가 받아오기
     }
 
     public void updateCommunityArticle(String title, String content, String imageUrl){
         this.title = title;
         this.content = content;
         this.imageUrl = imageUrl;
+        if (status != null) {
+            this.status = status;
+        }
     }
 }
 
