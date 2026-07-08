887ab361af5e9dbd326d03ab731e26fbaf0da6bd
JeUk Park <77144006+sleo113@users.noreply.github.com>
2026-07-08 10:27:56 +0900

    feat: 소셜 로그인을 위한 UserRepository 수정

--- src/main/java/com/ll/projectLimC/domain/user/repository/UserRepository.java
+++ src/main/java/com/ll/projectLimC/domain/user/repository/UserRepository.java
@@ -7,4 +7,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email); // email로 사용자 정보를 가져옴.
+
+    // 소셜 계정 중복 가입 체크 및 대조를 위한 핵심 쿼리
+    Optional<User> findByProviderAndProviderId(String provider, String providerId);
 }
