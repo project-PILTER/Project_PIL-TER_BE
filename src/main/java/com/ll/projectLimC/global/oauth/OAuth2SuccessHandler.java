0bc9ccf7cf7433d255ac334d12e05d9f2c07ef15
JeUk Park <77144006+sleo113@users.noreply.github.com>
2026-07-09 14:38:09 +0900

    fix: 소셜 로그인 리다이렉션 문제로 인한 OAuth2SuccessHandler 수정

--- src/main/java/com/ll/projectLimC/global/oauth/OAuth2SuccessHandler.java
+++ src/main/java/com/ll/projectLimC/global/oauth/OAuth2SuccessHandler.java
@@ -34,21 +34,30 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
-                                   HttpServletResponse response,
-                                   Authentication authentication) throws IOException {
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+
+        // 이미 CustomService에서 완벽하게 파싱해서 저장한 유저 객체(DefaultOAuth2User)를 꺼냅니다.
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-        User user = userService.findByEmail((String) oAuth2User.getAttributes()
-                .get("email"));
+
+        // 카카오, 구글 등 공급자 파편화 없이 OAuth2UserCustomService에서 연동해둔 이메일을 정석대로 추출
+        String email = (String) oAuth2User.getAttributes().get("email");
+
+        // 만약 카카오 이메일 추출 구조가 꼬일 것을 대비해, 안전하게 DB에서 다시 긁어오도록 연동
+        User user = userService.findByEmail(email);
 
         // 리프레시 토큰 생성 -> 저장 -> 쿠키에 저장
         String refreshToken = tokenProvider.generateToken(user, REFRESH_TOKEN_DURATION);
         saveRefreshToken(user.getId(), refreshToken);
         addRefreshTokenToCookie(request, response, refreshToken);
+
         // 액세스 토큰 생성 -> 패스에 액세스 토큰 추가
         String accessToken = tokenProvider.generateToken(user, ACCESS_TOKEN_DURATION);
         String targetUrl = getTargetUrl(accessToken);
+
         // 인증 관련 설정값, 쿠키 제거
         clearAuthenticationAttributes(request, response);
+
         // 리다이렉트
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
