a7e4b262e629aa7549455deb59ad0bba96645aa8
JeUk Park <77144006+sleo113@users.noreply.github.com>
2026-07-27 11:39:50 +0900

    fix: 건강일지 관련 Request 파일 수정

--- src/main/java/com/ll/projectLimC/domain/healthJournal/dto/HealthJournalRequest.java
+++ src/main/java/com/ll/projectLimC/domain/healthJournal/dto/HealthJournalRequest.java
@@ -1,5 +1,6 @@
 package com.ll.projectLimC.domain.healthJournal.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ll.projectLimC.domain.healthJournal.ConditionStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
@@ -16,12 +17,14 @@ import java.util.List;
 @NoArgsConstructor
 @Schema(description = "건강일지 조회 요청 정보 폼")
 public class HealthJournalRequest {
-    @CreatedDate
+
+    // 1. @CreatedDate 제거
+    // 2. pattern에 선택적 시간/타임존 구문([ '00:00:00'Z]) 추가
     @Schema(description = "일지 기록 날짜 (ISO 표준 날짜 포맷)", example = "2026-06-24")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd[ '00:00:00'Z]", timezone = "Asia/Seoul")
     private OffsetDateTime journalDate;
 
     @Schema(description = "오늘의 전반적인 컨디션 상태 (GOOD, NORMAL, BAD)", example = "GOOD")
-    // 프론트가 "GOOD"이라고 보내면 자동으로 Enum 매핑됨
     private ConditionStatus conditionStatus;
 
     @Schema(description = "통증 점수 (0: 통증 없음 ~ 10: 극심한 통증)", example = "3")
