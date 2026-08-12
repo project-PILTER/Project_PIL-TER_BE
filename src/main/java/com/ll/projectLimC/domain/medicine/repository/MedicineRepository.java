50197348d0fc95aaecbb3fbbab7d583f4f5810fd
JeUk Park <77144006+sleo113@users.noreply.github.com>
2026-08-12 12:27:47 +0900

    feat: 약 정보/후기 - Medicine 파일 구현

--- src/main/java/com/ll/projectLimC/domain/medicine/repository/MedicineRepository.java
+++ src/main/java/com/ll/projectLimC/domain/medicine/repository/MedicineRepository.java
@@ -4,4 +4,5 @@ import com.ll.projectLimC.domain.medicine.Entity.Medicine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicineRepository extends JpaRepository<Medicine, Long> {
+
 }
