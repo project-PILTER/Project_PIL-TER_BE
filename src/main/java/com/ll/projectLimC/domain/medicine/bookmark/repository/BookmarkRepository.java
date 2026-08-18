8400749444a6acafb34e36b36cdcdc45b21ae694
JeUk Park <77144006+sleo113@users.noreply.github.com>
2026-08-18 11:22:46 +0900

    feat: 약 후기 - BookmarkRepository 생성

---
+++ src/main/java/com/ll/projectLimC/domain/medicine/bookmark/repository/BookmarkRepository.java
@@ -0,0 +1,7 @@
+package com.ll.projectLimC.domain.medicine.bookmark.repository;
+
+import com.ll.projectLimC.domain.medicine.bookmark.entity.Bookmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class BookmarkRepository extends JpaRepository<Bookmark, Long> {
+}
